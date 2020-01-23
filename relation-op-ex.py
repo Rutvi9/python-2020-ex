8560c1f817e2d21d5aad59308637f63d3d8336d6
Rutvi9 <patelrutvi1101@gmail.com>
2020-01-23 15:45:26 +0530

    third commit

--- relation-op-ex.py
+++ relation-op-ex.py
@@ -4,9 +4,14 @@ print(a < b)
 print(a <= b)
 print(a > b)
 print(a >= b)
-
+print('------------------------------------')
 s1= 'a'
 s2= 'b'
 print(ord('a'))
 print(chr(98))
 print(s1 < s2)
+print('------------------------------------')
+s1= 'python'
+s2= 'python'
+print(s1 < s2)
+print(s1 <= s2)
