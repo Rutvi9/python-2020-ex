c759567a662e8a290ad17250265271c890a5ac80
Rutvi9 <patelrutvi1101@gmail.com>
2020-01-27 14:23:36 +0530

    first commit 27 jan

---
+++ Assignments/no.-betwn1to100.py
@@ -0,0 +1,5 @@
+a= int(input('enter number'))
+if a >=1 and a<=100 :
+    print('no. is in between 1 and 100',format(a))
+else:
+    print('no. is not in between 1 and 100', format(a))
---
+++ Assignments/odd_even_ex.py
@@ -0,0 +1,5 @@
+n= int(input('enter no.'))
+if (n%2) == 0 :
+    print('no is even',format(n))
+else:
+    print('no is odd',format(n))
